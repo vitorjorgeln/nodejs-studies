a830fea899efd960d36ccc7d175544b378394d86
Vitor Leal <vitorjorgeln@gmail.com>
2023-09-21 01:38:57 -0300

    Creating users, saving them in  memory and using header to return a JSON

--- 01-nodejs-fundamentals/src/server.js
+++ 01-nodejs-fundamentals/src/server.js
@@ -1,12 +1,23 @@
 import http from 'node:http'
+
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
    if (method == 'GET' && url == '/users'){
-    return res.end('GET users')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
    }
 
    if (method == 'POST' && url == '/users'){
+    users.push({
+        id: 1,
+        name: 'tojorge',
+        email: 'vitor.teste@email.com',
+        age: 24
+    })
     return res.end('CREATE users')
    }
 
