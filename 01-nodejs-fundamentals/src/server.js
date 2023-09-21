49621d64e07718838133726191795e0ec41a0d8e
Vitor Leal <vitorjorgeln@gmail.com>
2023-09-21 01:21:30 -0300

    created user creation and listing routes

--- 01-nodejs-fundamentals/src/server.js
+++ 01-nodejs-fundamentals/src/server.js
@@ -1,7 +1,16 @@
 import http from 'node:http'
+const server = http.createServer((req, res) => {
+    const { method, url } = req
 
-const server = http.createServer ((req,res) => {
+   if (method == 'GET' && url == '/users'){
+    return res.end('GET users')
+   }
+
+   if (method == 'POST' && url == '/users'){
+    return res.end('CREATE users')
+   }
+
     return res.end('Server is running on port 3333')
 })
 //localhost:3333
-server.listen(3333)
+server.listen(3333) 
