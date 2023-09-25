48a8e1f861eb9abf6858687a01303bb6d908e815
Vitor Leal <vitorjorgeln@gmail.com>
2023-09-24 22:23:17 -0300

    Readable, Writeable and Transform Streams fundamentals

--- 01-nodejs-fundamentals/streams/fundamentals.js
+++ 01-nodejs-fundamentals/streams/fundamentals.js
@@ -1,3 +1,37 @@
-// Ler pequenas partes de algo, sem que ele carregue por completo, e já conseguir trabalhar encima disso
+import { Readable, Writable, Transform } from 'node:stream'
 
-// Importação de clientes via CSV
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+        let i = this.index++
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                let buffer = Buffer.from(String(i))
+                this.push(buffer)
+            }
+        }, 1000);
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+class InvertPositiveToNegativeStream extends Transform {
+    _transform(chunk, encoding, callback){
+        let negativeNumber = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(negativeNumber)))
+    }
+}
+
+
+new OneToHundredStream()
+.pipe(new InvertPositiveToNegativeStream())
+.pipe(new MultiplyByTenStream())
