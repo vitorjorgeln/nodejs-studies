ad62e67da6cee0e7099ed25e4096e4f2832162ef
Vitor Leal <vitorjorgeln@gmail.com>
2023-10-01 01:45:51 -0300

    Implementing the request body in json

--- 01-nodejs-fundamentals/src/server.js
+++ 01-nodejs-fundamentals/src/server.js
@@ -2,24 +2,40 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-   if (method == 'GET' && url == '/users'){
-    return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users))
-   }
+    const buffers = []
 
-   if (method == 'POST' && url == '/users'){
-    users.push({
-        id: 1,
-        name: 'tojorge',
-        email: 'vitor.teste@email.com',
-        age: 24
-    })
-    return res.writeHead(201).end()
-   }
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        let body
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch (error) {
+        req.body = null
+    }
+    
+
+    if (method == 'GET' && url == '/users') {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method == 'POST' && url == '/users') {
+        const { name, email, age } = req.body
+
+        users.push({
+            id: 1,
+            name,
+            email,
+            age
+        })
+        return res.writeHead(201).end()
+    }
 
     return res.writeHead(404).end('Method or route not found.')
 })
