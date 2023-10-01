5987c95640df4ad488403811f42f8036a8a47fb9
Vitor Leal <vitorjorgeln@gmail.com>
2023-10-01 02:01:57 -0300

    Getting started with JSON  middlewares

---
+++ 01-nodejs-fundamentals/src/middlewares/json.js
@@ -0,0 +1,17 @@
+//Middleware é um "interceptador"
+// Função que intercepta a requisição 
+export async function json(req,res) {
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch (error) {
+        req.body = null
+    }
+
+    res.setHeader('Content-type', 'application/json')
+}
