e228732ffd7811f2a6c76302f005ebc4c21afaa4
Vitor Leal <vitorjorgeln@gmail.com>
2023-10-16 11:14:55 -0300

    Delete method created

--- 01-nodejs-fundamentals/src/database.js
+++ 01-nodejs-fundamentals/src/database.js
@@ -35,4 +35,12 @@ export class Database {
         let data = this.database[table] ?? []
         return data
     }
+
+    delete(table,id) {
+        let rowIndex = this.database[table].findIndex(row => row.id === id)
+        if(rowIndex > -1){
+            this.database[table].splice(rowIndex, 1)
+            this.persist()
+        }
+    }
 }
--- 01-nodejs-fundamentals/src/routes.js
+++ 01-nodejs-fundamentals/src/routes.js
@@ -2,11 +2,12 @@
 import { Database } from './database.js'
 let database = new Database()
 import {randomUUID} from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users')
             return res.end(JSON.stringify(users))
@@ -14,7 +15,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email, age } = req.body
             const user = {
@@ -26,5 +27,15 @@ export const routes = [
             database.insert('users', user)
             return res.writeHead(201).end()
         }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('users', id)
+            return res.writeHead(204).end()
+        },
+
     }
 ]
