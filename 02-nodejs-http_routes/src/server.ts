f425968d88e06b077c75096628a0e5c9a7a6625d
Vitor Leal <vitorjorgeln@gmail.com>
2024-01-05 00:33:07 -0300

    setting up eslint and creating the project structure

--- 02-nodejs-http_routes/src/server.ts
+++ 02-nodejs-http_routes/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World!!'
+  return 'Hello World!!'
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('*** Server is running! ***')
-})
+  })
