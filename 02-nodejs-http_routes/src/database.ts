42af646494b5acdc744a6790b68595c9700614cf
Vitor Leal <vitorjorgeln@gmail.com>
2024-01-09 00:08:48 -0300

    setting up knex migrations folder

--- 02-nodejs-http_routes/src/database.ts
+++ 02-nodejs-http_routes/src/database.ts
@@ -1,9 +1,13 @@
-import { knex as setupKnex } from 'knex'
-export const configDb = {
+import { knex as setupKnex, Knex } from 'knex'
+export const configDb: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 }
 export const knex = setupKnex(configDb)
